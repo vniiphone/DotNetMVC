13307f65e346cb758109cdadad58cf5193a59623
Vu Nguyen <vuky12@icloud.com>
2023-03-12 16:50:57 +0700

    Update _Layout Admin

--- DotNetMVC/Program.cs
+++ DotNetMVC/Program.cs
@@ -1,5 +1,6 @@
 using DotNetMVC.Data;
 using EmailService;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -46,9 +47,16 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+        name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.MapRazorPages();
 
