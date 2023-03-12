900c2975dcee973b4cbfc2b86a0f12557b0abc26
BinhNam123cm <binhnam123cm@gmail.com>
2023-03-12 10:23:44 +0700

    Models - Migration

---
+++ DotNetMVC/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetMVC.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CateId { get; set; }
+        public string CateName { get; set; } = null!;
+        public string? CateDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ DotNetMVC/Models/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetMVC.Models
+{
+    public partial class Supplier
+    {
+        public int SupId { get; set; }
+        public string SupPhone { get; set; } = null!;
+        public string SupName { get; set; } = null!;
+        public string SupEmail { get; set; } = null!;
+        public int AddId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public virtual Address Add { get; set; } = null!;
+    }
+}
